1042d136a7e517041b01bfa850d5293dda94e0d0
HyeongChang Lee <hc9904@naver.com>
2019-05-15 20:57:39 +0900

    Fix delete load

--- src/component/DeleteButton.js
+++ src/component/DeleteButton.js
@@ -2,17 +2,34 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
+import { connect } from "react-redux";
+import * as listActions from "store/modules/lists";
+import { bindActionCreators } from "redux";
+
 class DeleteButton extends Component {
   constructor(props) {
     super(props);
     this.Delete = this.Delete.bind(this);
+    this.Load = this.Load.bind(this);
   }
 
   Delete() {
     let storageUrl = this.props.url;
-    chrome.storage.sync.remove(encodeURIComponent(storageUrl), function() {
+    chrome.storage.sync.remove(encodeURIComponent(storageUrl), () => {
       console.log(storageUrl, "Deleted!");
+      this.Load();
     });
+  }
+
+  Load() {
+    chrome.storage.sync.get(
+      null,
+      function(storageList) {
+        console.log("Loding StorageList!");
+        const { ListActions } = this.props;
+        ListActions.set(storageList);
+      }.bind(this)
+    );
   }
 
   render() {
@@ -24,4 +41,11 @@ class DeleteButton extends Component {
   }
 }
 
-export default DeleteButton;
+export default connect(
+  value => ({
+    storageList: value.lists.get("all")
+  }),
+  dispatch => ({
+    ListActions: bindActionCreators(listActions, dispatch)
+  })
+)(DeleteButton);
